07fc1e9295d132f23d01de870718d7d98d942611
Martin Barreau <martin@thedeepsense.co>
2022-04-22 16:10:18 +0200

    feat(example): examples updated with the new public API

---
+++ example/manual_http_handler.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import "dart:io";
+
+import "package:stator/stator.dart";
+
+Future<HttpContext> test(HttpContext ctx, Map<String, String> params) async {
+  return ctx..sendJson({ "test": "hello" });
+}
+
+Future<HttpContext> testEcho(HttpContext ctx, Map<String, String> params) async {
+  if (!params.containsKey("echo")) {
+    return ctx..send(value: { "error": "Missing echo parameter" }, status: HttpStatus.badRequest);
+  }
+  return ctx..sendJson({ "value": params["echo"] });
+}
+
+Future<void> main() async {
+  Map<String, String> env = Platform.environment;
+  int httpPort = env.containsKey("PORT") ? int.parse(env["PORT"]!) : 9000;
+  HttpServer server = await HttpServer.bind(InternetAddress.anyIPv4, httpPort);
+
+  unawaited(ProcessSignal.sigint.watch().first.then((_) async {
+    print("Shutting down the server");
+    await server.close();
+  }));
+
+   RequestHandler engine = router({
+    "GET@/": test,
+    "GET@/hello/:echo": testEcho,
+  });
+
+  print("Server running on port ${server.port}");
+
+  await server.forEach((request) async {
+    HttpContext ctx = HttpContext(request, request.response);
+    HttpContext res = await engine(ctx);
+
+    if (!res.closed) {
+      await res.end();
+    }
+  });
+}
